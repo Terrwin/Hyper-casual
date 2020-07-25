0e5ccc14094e49574ac9f9e9c4fec934fb72e269
Terrwin <58670399+Terrwin@users.noreply.github.com>
2020-07-25 12:57:30 +0300

    0.0.0.1

---
+++ Huper casual 1/Assets/Scripts/Inheritance.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inheritance : MonoBehaviour
+{
+    private float a = 1f;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    public virtual void Jump()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y + a, transform.position.z);
+        
+    }
+}
---
+++ Huper casual 1/Assets/Scripts/ScphereMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScphereMovement : MonoBehaviour
+{
+    public int stars = 0;
+    public Transform camera;
+    private Rigidbody rb;
+    private bool isGrounded = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        rb.AddForce(Vector3.left * 100f * Time.deltaTime);
+        camera.transform.position = new Vector3(transform.position.x + 10, transform.position.y + 10, transform.position.z);
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * 500f);
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && !isGrounded)
+        {
+             rb.AddForce(Vector3.down * 500f);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other) 
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit(Collision other) 
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        if (other.gameObject.tag == "Finish")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        if (other.gameObject.tag == "Star")
+        {
+            stars += 1;
+            Destroy(other.gameObject);
+        }
+    }
+}
